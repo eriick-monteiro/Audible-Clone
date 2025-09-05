a62ca2418661d120531818002f935615c99a7f75
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-05 17:19:03 -0300

    :bricks: ci: Invertendo Botão Avançar 15 Segundos

--- src/BotoesControle.jsx
+++ src/BotoesControle.jsx
@@ -8,7 +8,7 @@ const BotoesControle = (props) => {
                 <i className={`bi bi-${props.isPlaying ? "pause" : "play"}-circle-fill`}></i>
             </button>
 
-            <button onClick={props.avancar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
+            <button className="invert" onClick={props.avancar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
             <button onClick={props.avancarFaixa}><i className="bi bi-skip-end"></i></button>
         </div>
     );
