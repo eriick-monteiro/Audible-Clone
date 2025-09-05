13da7ec0a7bb1b5df2b4bd7365fd147f574adae7
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-05 11:16:54 -0300

    :bug: fix Áudio não estava tocando por conta do onClick

--- src/BotoesControle.jsx
+++ src/BotoesControle.jsx
@@ -1,22 +1,15 @@
-const BotoesControle = (
-    retrocederFaixa,
-    voltar15s,
-    tocarOuPausarFaixa,
-    isPlaying,
-    avancar15s,
-    avancarFaixa
-) => {
+const BotoesControle = (props) => {
     return (
         <div className="caixa-botoes">
-            <button onClick={retrocederFaixa}><i className="bi bi-skip-start"></i></button>
-            <button onClick={voltar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
+            <button onClick={props.retrocederFaixa}><i className="bi bi-skip-start"></i></button>
+            <button onClick={props.voltar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
 
-            <button onClick={tocarOuPausarFaixa}>
-                <i className={`bi bi-${isPlaying ? "pause" : "play"}-circle-fill`}></i>
+            <button onClick={props.tocarOuPausarFaixa}>
+                <i className={`bi bi-${props.isPlaying ? "pause" : "play"}-circle-fill`}></i>
             </button>
 
-            <button onClick={avancar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
-            <button onClick={avancarFaixa}><i className="bi bi-skip-end"></i></button>
+            <button onClick={props.avancar15s}><i className="bi bi-arrow-counterclockwise"></i></button>
+            <button onClick={props.avancarFaixa}><i className="bi bi-skip-end"></i></button>
         </div>
     );
 }
